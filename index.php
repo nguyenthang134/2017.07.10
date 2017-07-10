8978250eef2cf39a88c716066f410a20f1b69cbb
Thắng <thang@NAL04.local>
2017-07-10 15:03:22 +0700

    HW

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Current server time</title>
+</head>
+<body>
+    <h1>Current server time is: <?php echo date('Y-M-d h:m:s'); ?></h1>
+</body>
+</html>
+
